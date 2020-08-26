c7fffaf6f7f7c64f4953aa88c4523c0c257e9558
malekshmes <malekshmes@gmail.com>
2020-08-26 13:02:02 +0300

    function

--- home.js
+++ home.js
@@ -1,18 +1,24 @@
-var name;
-name = prompt('what is your name?')
-var hight; 
+var name = prompt('what is your name?')
+var hight;
 var gender;
 console.log('the name',name);
 console.log('the hight',hight);
 console.log('the gender',gender);
+ 
+if(name=='malek')
+function verifying(){
+//input
+  hight = prompt('what is your hight?')
+  //proccess
+  if(hight >= 180){
+   
+   document.write(' <h4> your are allowed <h4> ')
+  }
+  else {
+    alert('you are not allowed to enter');
+  }
+//output 
+return verifying
 
-if(name=='malek') 
-prompt ('what is your hight?') 
-
-else {
-  alert('you are not allowed to enter');
-}
-
-{
-    alert('you have addmision ');
-}
+} 
+verifying();
