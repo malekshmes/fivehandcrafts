a13f5efb3f2f95d4eb95d6063298490b5e9f1683
malekshmes <malekshmes@gmail.com>
2020-08-26 03:25:31 +0300

    js edits

---
+++ home.js
@@ -0,0 +1,18 @@
+var name;
+name = prompt('what is your name?')
+var hight; 
+var gender;
+console.log('the name',name);
+console.log('the hight',hight);
+console.log('the gender',gender);
+
+if(name=='malek') 
+prompt ('what is your hight?') 
+
+else {
+  alert('you are not allowed to enter');
+}
+
+{
+    alert('you have addmision ');
+}
